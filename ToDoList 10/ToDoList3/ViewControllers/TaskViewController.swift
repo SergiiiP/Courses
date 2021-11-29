2c498a9bdec0a3803798c33a8f9863eb427f53bc
Sergii <mactenovavirtute@protonmail.com>
2021-11-29 16:38:53 +0200

    Task 16 complete

--- ToDoList 10/ToDoList3/ViewControllers/TaskViewController.swift
+++ ToDoList 10/ToDoList3/ViewControllers/TaskViewController.swift
@@ -7,12 +7,47 @@
 
 import UIKit
 
-class TaskViewController: BaseViewController {
-
+class TaskViewController: BaseViewController, UITextFieldDelegate, UITextViewDelegate {
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        titleTextField.delegate = self
+        descriptionTextView.delegate = self
+        
+        let tapGesture = UITapGestureRecognizer.init(target: self, action: #selector(hideKeyboard))
+        
+        view.isUserInteractionEnabled = true
+        view.addGestureRecognizer(tapGesture)
+        
+        
+        let editTextView = UITapGestureRecognizer.init(target: self, action: #selector(editTextView))
+        
+        descriptionTextView.isUserInteractionEnabled = true
+        descriptionTextView.addGestureRecognizer(editTextView)
+        
+    }
+    
+    @objc func hideKeyboard(){
+        view.endEditing(true)
+    }
+    
+    @objc func editTextView() {
+        descriptionTextView.isEditable = true
+        descriptionTextView.becomeFirstResponder()
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        view.endEditing(true)
+        return true
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView){
+        textView.isEditable = false
     }
     
 
